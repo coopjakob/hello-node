51449be2cba13b127b2fa8e622c77101e810006c
Jakob Nanneson <jakob.nanneson@coop.se>
2020-01-12 11:30:48 +0100

    init

---
+++ express.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 8080 //process.env.PORT
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(express.static('public'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
